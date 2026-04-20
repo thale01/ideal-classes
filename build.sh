3b14cf4ee31f1274a1112c9c0228405b37075e75
thale01 <siddhithale01@gmail.com>
2026-04-20 16:41:04 +0530

    Automate superuser creation in build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser for Free tier (using environment variables)
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser --noinput || true
+fi
